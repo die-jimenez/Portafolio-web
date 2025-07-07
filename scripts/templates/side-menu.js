265263edc2971ea68caaf9e27db66a4276914eaa
Diego Jiménez <diegojimenez2010@gmail.com>
2025-07-07 14:26:36 -0300

    Modificando rutas

--- scripts/templates/side-menu.js
+++ scripts/templates/side-menu.js
@@ -9,19 +9,35 @@ class SideMenu extends HTMLElement {
   }
 
   connectedCallback() {
+    const prefix = this.caluclatePrefix();
+
     this.innerHTML = `
           <nav class="side-menu">
             <ul>
-              <li><a href="/index.html" class="menu-item">Sobre mi</a></li>
-              <li><a href="/pages/experiencia.html" class="menu-item">Experiencia</a></li>
-              <li><a href="/pages/portafolio.html" class="menu-item">Portafolio</a></li>
-              <li><a href="/pages/educacion.html" class="menu-item">Educacion</a></li>
-              <li><a href="/pages/habilidades.html" class="menu-item">Habilidades</a></li>
-              <li><a href="/pages/otrosProyectos.html"class="menu-item">Otros proyectos</a></li>
+              <li><a href="${prefix}index.html" class="menu-item">Sobre mi</a></li>
+              <li><a href="${prefix}pages/experiencia.html" class="menu-item">Experiencia</a></li>
+              <li><a href="${prefix}pages/portafolio.html" class="menu-item">Portafolio</a></li>
+              <li><a href="${prefix}pages/educacion.html" class="menu-item">Educacion</a></li>
+              <li><a href="${prefix}pages/habilidades.html" class="menu-item">Habilidades</a></li>
+              <li><a href="${prefix}pages/otrosProyectos.html"class="menu-item">Otros proyectos</a></li>
             </ul>
           </nav>`;
 
     this._updateActive();
+  }
+
+  //Calcula cuantas veces debe retroceder la ruta para hallar la directoria correcta. Asi puedo usar rutas """absolutas"""
+  caluclatePrefix() {
+    const isInPages = window.location.pathname.includes('/pages/');
+    const isInProjectNotes = window.location.pathname.includes('/project-notes/');
+
+    if (isInPages && isInProjectNotes) {
+      return '../../';
+    } else if (isInPages) {
+      return '../';
+    } else {
+      return '';
+    }
   }
 
   //Es una especie de listener por si algún js cambia el atributo 'active'
@@ -35,7 +51,7 @@ class SideMenu extends HTMLElement {
     // quitamos active de todas las opciones
     const options = this.querySelectorAll('a.menu-item');
     options.forEach(a => a.classList.remove('active'));
-    
+
     // marcamos la que corresponde (1‑based)
     const i = parseInt(this.getAttribute('active'), 10);
     if (!isNaN(i) && i >= 0 && i <= options.length) {
